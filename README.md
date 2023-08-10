eca9731d3533536ebe30bbc597826b8bb3e0056d
Fidel Reyes <freyesllerena33@gmail.com>
2023-08-10 12:44:59 +0200

    cleanup test jeu de cartes

---
+++ README.md
@@ -0,0 +1,7 @@
+prerequis
+5.4.18
+Symfony version
+8.0.28
+PHP version
+
+route du test: /card/rand
---
+++ src/Controller/CardController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Constant\Card;
+
+class CardController extends AbstractController
+{
+    /**
+     * @Route("/")
+     * @return Response
+     */
+    public function homepage(): Response
+    {
+        return $this->render('cards/homepage.html.twig');
+    }
+
+    /**
+     * @Route("/card/rand")
+     * @return Response
+     */
+    public function randCard(): Response
+    {
+        $cards = Card::mainCards();
+
+        return $this->render('cards/rand.html.twig', [
+            'cards' => $cards['cards'],
+            'ascCards' => $cards['asc-cards']
+        ]);
+    }
+}
