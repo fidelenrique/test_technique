8eab8ba4f726d0b60e32957b2317e5518ced1d04
Fidel Reyes <freyesllerena33@gmail.com>
2023-08-26 16:48:57 +0200

    factorization du code test jeu de cartes

--- src/Constant/Card.php
+++ src/Constant/Card.php
@@ -4,6 +4,8 @@ namespace App\Constant;
 
 abstract class Card
 {
+    const COLORS_CARD = ['carreau', 'coeur', 'pique', 'trefle'];
+
     public static function randCards(): array
     {
         return [rand(1, 13) => array_rand(['carreau' => 1, 'coeur' => 2, 'pique' => 3, 'trefle' => 4])
@@ -64,73 +66,30 @@ abstract class Card
     {
         asort($colorsAsc);
         $orderColorsAcs = [];
+        $resultColors = [];
         // 6 browse sort by value
         foreach ($colorsAsc as $color) {
 
-            if ($color === 'carreau') {
-                // 7 go through the 10 cards to find the carreau
-                for ($i = 0; $i < 10; $i++) {
-                    if (isset($cards[$i])) {
-                        if(key(array_flip($cards[$i])) === $color) {
-                            $orderColorsAcs[$color][] = key($cards[$i]);
+            $resultColors = [];
+
+            foreach (Card::COLORS_CARD as $colorCard) {
+                if ($color === $colorCard) {
+                    // 7 go through the 10 cards to find the carreau
+                    for ($i = 0; $i < 10; $i++) {
+                        if (isset($cards[$i])) {
+                            if(key(array_flip($cards[$i])) === $color) {
+                                $orderColorsAcs[$color][] = key($cards[$i]);
+                            }
                         }
                     }
                 }
-            }
-            if ($color === 'coeur') {
-                // 8 go through the 10 cards to find the coeur
-                for ($i = 0; $i < 10; $i++) {
-                    if (isset($cards[$i])) {
-                        if(key(array_flip($cards[$i])) === $color) {
-                            $orderColorsAcs[$color][] = key($cards[$i]);
-                        }
+
+                if (isset($orderColorsAcs[$colorCard])) {
+                    if (!empty($orderColorsAcs[$colorCard])) {
+                        $resultColors[] = [$colorCard => array_unique($orderColorsAcs[$colorCard])];
                     }
                 }
-            }
 
-            if ($color === 'pique') {
-                // 9 go through the 10 cards to find the pique
-                for ($i = 0; $i < 10; $i++) {
-                    if (isset($cards[$i])) {
-                        if(key(array_flip($cards[$i])) === $color) {
-                            $orderColorsAcs[$color][] = key($cards[$i]);
-                        }
-                    }
-                }
-            }
-            if ($color === 'trefle') {
-                // 10 go through the 10 cards to find the trefle
-                for ($i = 0; $i < 10; $i++) {
-                    if (isset($cards[$i])) {
-                        if(key(array_flip($cards[$i])) === $color) {
-                            $orderColorsAcs[$color][] = key($cards[$i]);
-                        }
-                    }
-                }
-            }
-
-        }
-
-        $resultColors = [];
-
-        if (isset($orderColorsAcs['carreau'])) {
-            if (!empty($orderColorsAcs['carreau'])) {
-                $resultColors[] = ['carreau' => array_unique($orderColorsAcs['carreau'])];
-            }
-        }
-        if (isset($orderColorsAcs['coeur'])) {
-            if (!empty($orderColorsAcs['coeur'])) {
-                $resultColors[] = ['coeur' => array_unique($orderColorsAcs['coeur'])];
-            }
-        }
-        if (isset($orderColorsAcs['pique'])) {
-            if (!empty($orderColorsAcs['pique'])) {
-                $resultColors[] = ['pique' => array_unique($orderColorsAcs['pique'])];
-            }
-        }
-        if (isset($orderColorsAcs['trefle'])) {
-            if (!empty($orderColorsAcs['trefle'])) {
-                $resultColors[] = ['trefle' => array_unique($orderColorsAcs['trefle'])];
             }
         }
 
